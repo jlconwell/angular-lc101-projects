2283ce29977baf89b169921c5c975d66ca147065
Jake Conwell <jakelconwell@gmail.com>
2023-03-06 12:01:28 -0600

    completed exercise 1

--- lesson1/exercises/src/app/app.module.ts
+++ lesson1/exercises/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { ChoresListComponent } from './chores-list/chores-list.component';
 import { FavPhotosComponent } from './fav-photos/fav-photos.component';
+import { PageTitleComponent } from './page-title/page-title.component';
 import { FavLinksComponent } from './fav-links/fav-links.component';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { FavLinksComponent } from './fav-links/fav-links.component';
     MovieListComponent,
     ChoresListComponent,
     FavPhotosComponent,
+    PageTitleComponent,
     FavLinksComponent
   ],
   imports: [
